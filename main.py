1da791af52e77a569b7d8b5cda33c7417de70f84
Loi Nguyen <ltnguyen1995@gmail.com>
2021-11-08 05:46:44 +0000

    Add can construct

---
+++ main.py
@@ -0,0 +1,19 @@
+#return true or false if the string can be constructed
+def can_construct(target, wordBank, memo):
+  if target in memo:
+    return memo[target]
+  if target == "":
+    return True
+  for word in wordBank:
+    if word in target[0:len(word)]:
+      if can_construct(target[len(word):], wordBank, memo) == True:
+        memo[target] = True
+        return memo[target]
+  return False
+
+print(can_construct("string", ["st", "sh", "ng", "ir", "ri"], dict()))
+print(can_construct("string", ["st", "sh", "ng", "ir", "r"], dict()))
+
+print(can_construct("abcdef", ["ab", "abc", "cd", "def", "abcd"], dict()))
+print(can_construct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"], dict()))
+print(can_construct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"], dict()))
